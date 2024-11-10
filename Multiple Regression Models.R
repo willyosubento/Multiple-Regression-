c39e264697b8a8d3f4e692f53df576858e1df37f
willy osubento <147528658+willyosubento@users.noreply.github.com>
2024-11-10 10:58:26 +0300

    Add files via upload

---
+++ Multiple Regression Models.R
@@ -0,0 +1,9 @@
+#load the packages
+library(readr)
+library(dplyr)
+library(tidyr)
+library(tidyverse)
+
+#Question 5.22
+Emotional_data<-read.csv("C:\\Users\\user\\Desktop\\Emotional Intelligence Test.csv")
+view(Emotional_data)
